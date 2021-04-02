45be84781ad2a67bad4c134fa9a27938da29e91f
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-04-02 01:49:29 -0300

    Funcionalidad de listar movimientos por tipo y categoria

--- mi-presupuesto/src/app/components/movements-manager/movements-manager.component.ts
+++ mi-presupuesto/src/app/components/movements-manager/movements-manager.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { MovementsApiClient } from '../../services/movements-api-client.service';
 import { FormAddMovementComponent } from '../form-add-movement/form-add-movement.component';
+import { Category } from '../../models/category.model';
 
 @Component({
   selector: 'app-movements-manager',
@@ -9,13 +10,44 @@ import { FormAddMovementComponent } from '../form-add-movement/form-add-movement
   styleUrls: ['./movements-manager.component.css']
 })
 export class MovementsManagerComponent implements OnInit {
+	movements: any[];
+	categories: Map<number, string>;
+	categorySelected = 0;
+	typeSelected = "";
 
-  constructor(public movementsApiClient: MovementsApiClient, private modalService: NgbModal) { }
+  constructor(public movementsApiClient: MovementsApiClient, private modalService: NgbModal) {
+  	this.categories = Category.getCategories("");
+  	this.movements = movementsApiClient.getAllMovements();
+  }
 
   ngOnInit(): void {
   }
 
   add() {
   	const modalRef = this.modalService.open(FormAddMovementComponent);
+  }
+
+  typeSelection() {
+  	console.log("Se actualizo las categorias. Type:");
+  	console.log(this.typeSelected);
+  	this.categorySelected = 0;
+  	if(this.typeSelected == "") {
+  		this.movements = this.movementsApiClient.getAllMovements();
+  		this.categories = Category.getCategories("");
+  	}	else {
+	  	this.movements = this.movementsApiClient.getTypeList(this.typeSelected);
+	 		this.categories = Category.getCategories(this.typeSelected);
+	 	}
+  }
+
+  categorySelection() {
+  	if(this.categorySelected == 0) {
+  		if(this.typeSelected == "")
+	  		this.movements = this.movementsApiClient.getAllMovements();
+	  	else
+	  		this.movements = this.movementsApiClient.getTypeList(this.typeSelected);
+  	} else {
+  		this.movements = this.movementsApiClient.getCategoryList(this.categorySelected);
+  	}
   }
 }
